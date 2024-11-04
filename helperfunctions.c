0e54ab36f4180df54a37d48ac511c4a49d1d1ba9
Nick <nick.marietta@csu.fullerton.edu>
2024-11-04 10:20:58 -0800

    implemented changes with helper functions. execvp changed to execve.

---
+++ helperfunctions.c
@@ -0,0 +1,56 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void add_character_to_string(char *str, char c) {
+  int len = strlen(str);
+  str[len] = c;
+  str[len + 1] = '\0';
+}
+
+// splits string by spaces; adds a NULL into array after last word
+void break_into_words(char *cmd, char *words[], char delimiter) {
+  int word_count = 0;
+  char *next_char = cmd;
+  char current_word[10000];
+  strcpy(current_word, "");
+
+  while (*next_char != '\0') {
+    if (*next_char == delimiter) {
+      words[word_count++] = strdup(current_word);
+      strcpy(current_word, "");
+    } else {
+      add_character_to_string(current_word, *next_char);
+    }
+    ++next_char;
+  }
+  words[word_count++] = strdup(current_word);
+
+  words[word_count] = NULL;
+}
+
+bool find_absolute_path(char *cmd, char *absolute_path) {
+  // break path up by ":" and return true if found
+  char *directories[1000];
+
+  break_into_words(getenv("PATH"), directories, ':');
+
+  // look in array until i find the paththing+cmd
+  for (int ix = 0; directories[ix] != NULL; ++ix) {
+    char path[1000];
+    strcpy(path, directories[ix]);
+    add_character_to_string(path, '/');
+    strcat(path, cmd);
+
+    if (access(path, X_OK) == 0) {
+      strcpy(absolute_path, path);
+      // once i find it, return true
+      return true;
+    }
+  }
+  // not found, so return false
+  return false;
+}
--- redirect.c
+++ redirect.c
@@ -1,121 +1,90 @@
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "helperfunctions.c"
 
 // usage for base program: redir <inp> <cmd> <out>
 // assume values for input and output are filenames! read & write to them
 
-int main(int argc, char *argv[])
-{
-    if (argc < 4)
-    {
-        fprintf(stderr,
-                "Usage: %s redir <inp> <cmd> <out>...\n", argv[0]);
-        return 1;
-    }
+int main(int argc, char *argv[]) {
 
-    int input_fd;
-    if (strcmp(argv[1], "-") == 0)
-    {
-        // if "-", leave them as stdin, stdout
-        input_fd = STDIN_FILENO;
-    }
-    else
-    {
-        input_fd = open(argv[1], O_RDONLY);
-        if (input_fd == -1)
-        {
-            perror("Could not open input file");
-            return 1;
-        }
-    }
+  // argument error handling
+  if (argc < 4) {
+    fprintf(stderr, "Usage: %s redir <inp> <cmd> <out>...\n", argv[0]);
+    return 1;
+  }
 
-    int output_fd;
-    if (strcmp(argv[3], "-") == 0)
-    {
-        output_fd = STDOUT_FILENO;
+  int input_fd;
+  if (strcmp(argv[1], "-") == 0) {
+    // if "-", leave as stdin
+    input_fd = STDIN_FILENO;
+  } else {
+    input_fd = open(argv[1], O_RDONLY);
+    if (input_fd == -1) {
+      perror("Could not open input file");
+      return 1;
     }
-    else
-    {
-        // argv[3] since we are targeting the output file
-        output_fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-        if (output_fd == -1)
-        {
-            perror("Could not open output file");
-            return 1;
-        }
+  }
+
+  int output_fd;
+  // if it is a -, leave as stdout
+  if (strcmp(argv[3], "-") == 0) {
+    output_fd = STDOUT_FILENO;
+  } else {
+    // argv[3] since we are targeting the output file
+    output_fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (output_fd == -1) {
+      perror("Could not open output file");
+      return 1;
     }
+  }
 
-    // split the command into diff. arguments
-    char **newargv = (char **)malloc(sizeof(char *) * (argc - 2));
+  char absolute_path[1000];
+  char *cmd[1000];
 
-    // we start at 2 since we are not using the first 2 arguments
-    for (int ix = 2; ix < argc; ix++)
-    {
-        newargv[ix - 2] = argv[ix];
-    }
-    newargv[argc - 3] = NULL;
+  // split the command into diff. arguments (split by spaces)
+  break_into_words(argv[2], cmd, ' ');
 
-    /*
-    printf("Debug: Executing this command: %s\n", newargv[0]);
-    for (int ix = 0; newargv[ix] != NULL; ix++)
-    {
-        printf("Debug: newargv[%d] = %s\n", ix, newargv[ix]);
-    }
-    */
-
-    int child_pid = fork();
-    if (child_pid == -1)
-    {
-        perror("fork error");
-        return 1;
-    }
-    if (child_pid == 0)
-    {
-        // child process
-        if (input_fd != STDIN_FILENO)
-        {
-            dup2(input_fd, STDIN_FILENO);
-            close(input_fd);
-        }
-        if (output_fd != STDOUT_FILENO)
-        {
-            dup2(output_fd, STDOUT_FILENO);
-            close(output_fd);
-        }
-
-        // need absolute path for execve so we use execvp
-        execvp(newargv[0], newargv);
-        // permission denied here
-        /*for (int ix = 0; newargv[ix] != NULL; ix++)
-        {
-            printf("newargv[%d] = %s\n", ix, newargv[ix]);
-        }
-         HERE: added read and write permissions to files
-        */
-
-        perror("execvp failed to run command");
-        // exit the child process if execvp fails
-        exit(1);
-    }
-    if (input_fd != STDIN_FILENO)
-    {
-        close(input_fd);
-    }
-    if (output_fd != STDOUT_FILENO)
-    {
-        close(output_fd);
-    }
-
-    // wait for the child to finish
-    wait(NULL);
-
-    // free the allocated memory
-    free(newargv);
+  // check if path is messed up
+  if (!find_absolute_path(cmd[0], absolute_path)) {
+    fprintf(stderr, "Error: command not found %s\n", cmd[0]);
     return 0;
+  }
+
+  int child_pid = fork();
+  if (child_pid == -1) {
+    perror("fork error");
+    return 1;
+  }
+  if (child_pid == 0) {
+    // child process
+    if (input_fd != STDIN_FILENO) {
+      // this is like saying stdout = input_fd
+      dup2(input_fd, STDIN_FILENO);
+      close(input_fd);
+    }
+    if (output_fd != STDOUT_FILENO) {
+      dup2(output_fd, STDOUT_FILENO);
+      close(output_fd);
+    }
+
+    // NOTE: execvp would find the path too but we use execve
+    execve(absolute_path, cmd, NULL);
+
+    perror("execve failed to run command");
+    // exit the child process if execve fails
+    exit(1);
+  }
+
+  // wait for the child to finish
+  wait(NULL);
+
+  return 0;
 }
